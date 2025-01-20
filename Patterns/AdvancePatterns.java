27640b86c814f7f9f3f570d2a9eb87fe02d0e58f
AmanTiwari2304 <amant9363@gmail.com>
2025-01-20 12:24:07 +0530

    Advance Patterns

--- Patterns/AdvancePatterns.java
+++ Patterns/AdvancePatterns.java
@@ -89,6 +89,63 @@ public class AdvancePatterns {
         }
         
     }
+
+    public static void solidRhombus(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void hollowRhombus(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                if (i==1 || j==1 || i==n || j==n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+               
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void diamond(int n){
+        //1st half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2*i)-1; j++) {
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+
+        //2nd half
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2*i)-1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter no. of row you want : ");
@@ -101,6 +158,10 @@ public class AdvancePatterns {
         // invertedHalfPyramidWithNumber(n);
         // FroydsTringle(n);
         // oneZeroTriangle(n);
-        Butterfly(n);
+        // Butterfly(n);
+        // solidRhombus(n);
+        // hollowRhombus(n);
+        diamond(n) ;
+
     }
 }

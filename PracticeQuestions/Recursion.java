33e4e609a0310cfbcd0333c1ed3cd87287e5dc9c
AmanTiwari2304 <amant9363@gmail.com>
2025-02-13 14:43:19 +0530

    Practice Question of Recursion

--- PracticeQuestions/Recursion.java
+++ PracticeQuestions/Recursion.java
@@ -27,12 +27,26 @@ public class Recursion {
         return 1+ lengthOfString(str.substring(1));
     }
 
+    public static void towerOfHanoi(int n, String sour, String helper,String dest){
+        // Base 
+        if (n==1) {
+            System.out.println("Transfer disk " + n + " from " + sour + " to " + dest);
+            return;
+        }
+        towerOfHanoi(n-1, sour, dest, helper);
+        System.out.println("Transfer disk " + n + " from " + sour + " to " + dest);
+        towerOfHanoi(n-1, helper, sour, dest);
+    }
+
     public static void main(String[] args) {
         // int arr[] = {3, 2, 4, 5, 6, 2, 7, 2, 2};
         // int key = 2;
         // allOccurrences(arr, key, 0);
         // printDigit(1235);
-        String str = "Hello";
-        System.out.println(lengthOfString(str));
+        // String str = "Hello";
+        // System.out.println(lengthOfString(str));
+        int n = 3;
+        towerOfHanoi(n, "S", "H", "D");
+        
     }
 }

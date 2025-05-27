af1de1088e77fb1cbef40d3eb355eb17fe536841
AmanTiwari2304 <amant9363@gmail.com>
2025-05-27 16:22:54 +0530

    Trie

---
+++ Trie/Implimentation/Classroom.java
@@ -0,0 +1,36 @@
+package Trie.Implimentation;
+
+public class Classroom {
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false; //end of word
+        
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String words[]){
+        Node curr = root;
+        for (int level = 0; level < words.length; level++) {
+            int idx = words.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] == new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static void main(String[] args) {
+        String words[] = {"the", "a", "there", "their", "any", "thee"};
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+    }
+}

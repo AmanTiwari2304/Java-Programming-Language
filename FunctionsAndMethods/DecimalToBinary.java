bac3013d88590cc31e11014fd419863725d62540
AmanTiwari2304 <amant9363@gmail.com>
2025-06-25 16:08:19 +0530

    Graphs

--- FunctionsAndMethods/DecimalToBinary.java
+++ FunctionsAndMethods/DecimalToBinary.java
@@ -2,19 +2,31 @@ import java.util.*;
 
 public class DecimalToBinary {
     public static void DecToBin(int n){
-        int binNum = 0;
-        int pow = 0;
-        while (n > 0) {
-            int lastDigit = 0;
-            lastDigit = n % 2 ;
-            binNum = binNum + (lastDigit*(int)(Math.pow(10, pow)));
+        // int binNum = 0;
+        // int pow = 0;
+        // while (n > 0) {
+        //     int lastDigit = 0;
+        //     lastDigit = n % 2 ;
+        //     binNum = binNum + (lastDigit*(int)(Math.pow(10, pow)));
 
-            pow++;
-            n = n /2 ;
+        //     pow++;
+        //     n = n /2 ;
 
+        // }
+
+        // System.out.print(binNum);
+
+        String binary = "";
+        int num = n;
+
+        while (num > 0) {
+            int rem = num % 2;
+            binary = rem + binary;
+            num = num / 2;
         }
 
-        System.out.print(binNum);
+        System.out.println(binary);
+
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
